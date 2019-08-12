c2baed9aab29e34d93bde4870dc839d9fde970cb
Kevin Karma <kevinkarma55@gmail.com>
2019-08-12 00:33:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,6 @@ def adding_matz
                                             :known_for => "Ruby",  
                                             :languages => ["LISP", "C"]
     }
-    return programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
